8eef3c09c74648b71dfc8d6d2dbf912552eedd96
Britney Parkerson <justbritneyp1989@gmail.com>
2020-01-14 23:48:02 -0500

    added some commented out example code form codepen challenge

--- js/index.js
+++ js/index.js
@@ -40,3 +40,7 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//example selector
+// const logoTitle = document.getElementById("logo-title")
+// console.log('logo title', logoTitle)
